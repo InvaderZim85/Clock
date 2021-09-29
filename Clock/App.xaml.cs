a7999f5cae4fb674766002fa7cb166c2f94674e4
Andreas Pouwels <apouwels85@gmail.com>
2021-09-29 23:58:24 +0200

    initial commit

---
+++ Clock/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using Clock.Ui.View;
+
+namespace Clock
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Contains the main window
+        /// </summary>
+        private MainWindow _mainWindow;
+
+        /// <summary>
+        /// Occurs when the application starts
+        /// </summary>
+        /// <param name="sender">The <see cref="App"/></param>
+        /// <param name="e">The event arguments</param>
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            _mainWindow = new MainWindow();
+
+            var settings = Clock.Properties.Settings.Default;
+            if (settings.PosX == 0 || settings.PosY == 0)
+                _mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            else
+            {
+                _mainWindow.WindowStartupLocation = WindowStartupLocation.Manual;
+                _mainWindow.Left = settings.PosX;
+                _mainWindow.Top = settings.PosY;
+            }
+
+            _mainWindow.Show();
+        }
+
+        /// <summary>
+        /// Occurs when the application will closed
+        /// </summary>
+        /// <param name="sender">The <see cref="App"/></param>
+        /// <param name="e">The event arguments</param>
+        private void App_OnExit(object sender, ExitEventArgs e)
+        {
+            Clock.Properties.Settings.Default.PosX = _mainWindow?.Left ?? 0;
+            Clock.Properties.Settings.Default.PosY = _mainWindow?.Top ?? 0;
+            Clock.Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ Clock/Ui/View/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using Clock.Ui.ViewModel;
+using MahApps.Metro.Controls;
+
+namespace Clock.Ui.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="MainWindow"/>
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Occurs when the main window was loaded
+        /// </summary>
+        /// <param name="sender">The <see cref="MainWindow"/></param>
+        /// <param name="e">The event arguments</param>
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SetBlackoutDates();
+
+            if (DataContext is MainWindowViewModel viewModel)
+                viewModel.InitViewModel(SetBlackoutDates);
+        }
+
+        /// <summary>
+        /// Sets the blackout dates
+        /// </summary>
+        private void SetBlackoutDates()
+        {
+            CalendarView.BlackoutDates.AddDatesInPast();
+        }
+    }
+}
